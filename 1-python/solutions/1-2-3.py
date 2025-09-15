744220b04aec79f5ad5844a325bf55088bc0e621
Daniel Kane <dlkane@syr.edu>
2025-09-15 16:56:43 -0400

    commit

--- 1-python/solutions/1-2-3.py
+++ 1-python/solutions/1-2-3.py
@@ -4,4 +4,13 @@ Write a sentinel controlled loop to input a color until "quit" is entered. add e
 only when the color is not already in the list print the list each time in the loop.
 '''
 
-
+list = []
+while True:
+    color = input("Enter a color: ")
+    if color == 'quit':
+        break
+    if color in list:
+        print("You already entered that color")
+    else:
+        list.append(color)
+        print(list)
--- 1-python/solutions/1-2-4.py
+++ 1-python/solutions/1-2-4.py
@@ -6,3 +6,17 @@ loop until "quit" is entered. input a grocery item input a quantity save the ite
 and quantity as the value if the item is in the dictionary already, add the quantity to the existing value.
 
 '''
+
+items = {}
+
+while True:
+    item = input("Enter an item: ")
+    if item == 'quit':
+        print("Quitting...")
+        break
+    qty = int(input("Enter a quantity: "))
+    if item in items.keys():
+        items[item] = items[item] + qty
+
+        items[item] = qty
+        print('ITEMS:', items)
